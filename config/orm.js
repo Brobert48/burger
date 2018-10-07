d422cb034b7a77f7064885efc9c8877dd7fa348e
Blake Robert <Blaker.blaker4@gmail.com>
2018-10-07 18:04:06 -0400

    clean up code

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,5 @@
 var connection = require("./connection.js");
 
-// Object Relational Mapper (ORM)
-
-// The ?? signs are for swapping out table or column names
-// The ? signs are for swapping out other values
-// These help avoid SQL injection
-// https://en.wikipedia.org/wiki/SQL_injection
 
 function printQuestionMarks(num) {
   var arr = [];
@@ -20,22 +14,16 @@ function printQuestionMarks(num) {
 function objToSql(ob) {
   var arr = [];
 
-  // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
     var value = ob[key];
-    // check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
       arr.push(key + "=" + value);
     }
   }
 
-  // translate array of strings to a single comma-separated string
   return arr.toString();
 }
 
